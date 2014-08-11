1b442598dff9a354d3dd2927667e3ad492703f69
dalisaydavid <dalisay.david3@gmail.com>
2014-08-11 12:19:38 -0700

    TODO: Make sure this is the last spot in the array.
    TODO: Make sure this is the last spot in the array.

--- Yelp_KMeans/CoffeeShops/src/app/CoffeeShopTrendFinder.java
+++ Yelp_KMeans/CoffeeShops/src/app/CoffeeShopTrendFinder.java
@@ -78,7 +78,7 @@ public class CoffeeShopTrendFinder {
 		bpsAvail.remove(furthestBP1);
 		bpsAvail.remove(furthestBP2);
 		clusters[0] = new Cluster("cluster 0", furthestBP1); // Make furthest clusters first.
-		clusters[clusters.length-1] = new Cluster("cluster " + (clusters.length-1), furthestBP2);
+		clusters[clusters.length-1] = new Cluster("cluster " + (clusters.length-1), furthestBP2); // TODO: Make sure this is the last spot in the array.
 		for (int i = 1; i < clusters.length-1; i++){
 			clusters[i] = new Cluster("cluster "+i, getRandomBusinessPoint(bpsAvail)); // Gets random business points for the clusters that ARE NOT the furthest clusters.
 		}
